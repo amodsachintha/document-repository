3e649f106aeb4627418a7ba91e1e74b040c3072f
amodsachintha <amod077@gmail.com>
2018-09-03 12:39:38 +0530

    Auth scaffolding

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends("layouts.app")
+
+@section("content")
+    <div class="container" style="margin-top: 40px">
+        <div class="row">
+            <div class="col-md-4 offset-4 text-center">
+                <p>PLEASE LOGIN</p>
+            </div>
+        </div>
+    </div>
+@endsection
